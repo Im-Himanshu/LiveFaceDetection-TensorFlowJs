f2c2da221e2cae419303c96c50671eb3328a9ba0
ImHimanShoe <goyalhimanshu414@gmail.com>
2020-02-08 13:28:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const video = document.getElementById('video')
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./Face-Detection-JavaScript/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/Face-Detection-JavaScript/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
